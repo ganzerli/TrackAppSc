fbb671eb1afc20854ca4e1db3a77c068544c1250
Massimo Ganzerli <massimoganzerli@yahoo.com>
2019-01-10 18:59:54 +0100

    setiing up routes for login

--- src/api/users.js
+++ src/api/users.js
@@ -1,10 +1,19 @@
-app.get("/", (req, res) => {
-  res.render(path.resolve("dist"));
-  /*res.sendFile("index.html", {
-      root: path.resolve("dist")
-    });*/
+const express = require("express");
+const path = require("path");
+const router = express.Router();
+
+router.use(express.static(path.resolve(__dirname, "..", "..", "dist")));
+// needed to load css and the info in the dist folder
+router.get("/login", (req, res) => {
+  //res.render(path.resolve("dist"));
+  //res.json({ wow: "yes" });
+  res.sendFile("index.html", {
+    root: path.resolve("dist")
+  });
 });
 
-app.post("/", (req, res) => {
+router.post("/", (req, res) => {
   res.json({ response: "response" });
 });
+
+module.exports = router;
--- src/app.js
+++ src/app.js
@@ -3,10 +3,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const app = express();
+//importing other routes
+const users = require("./api/users");
+//const profiles = require("./src/api/profiles");
 // setup of app server
 app.use(bodyParser.json());
 // setup the engine of the index file
-app.set("view engine", "ejs");
+app.set("view engine", "html");
 // make this path visible for the app, to be included in routes and all, if not in same folder, specify
 app.use(express.static(path.resolve(__dirname, "..", "dist")));
 // the app knows that the dist folder is available to serve or render.. or res.res.render does not work
@@ -20,6 +23,10 @@ app.get("/", (req, res) => {
 app.post("/", (req, res) => {
   res.json({ response: "response" });
 });
+
+app.use("/api/users", users);
+
+//app.use("/api/prifile", profiles);
 //make app
 //feed app
 //export app
