2dd2d2d860159aa9dc972570d6ad4aa414aaa598
Massimo Ganzerli <massimoganzerli@yahoo.com>
2019-01-16 17:51:52 +0100

    deploying...

--- dist/js/pages/welcomePage.js
+++ dist/js/pages/welcomePage.js
@@ -1,30 +1,30 @@
 export const welcomePage = `
 <div class="welcome">
-<h2 class="welcome-tilte">Welcome to PRIVAPP</h2>
+<h2 class="welcome-tilte">Welcome to PRYVAPP</h2>
 
 <h4 class="welcome-subtitle">
   Here you can manage the organization of your days and save encrypted
-  messages wita key you choose your key
+  messages wit a key you choose!
 </h4>
 
 <p class="welcome-paragraph">
-  Add yout thoughts throught out the day if something you mind is to keep
+  Add yout thoughts throught out the day, if something you mind is to keep
   in the head, leave your ideas for later direcly fast and easy in your
-  dashboard, or encrypt your messages and info and send them to the
+  dashboard, or encrypt your messages and info and send them encrypted to the
   server. Your messages are for the entire process encrypted, from when
-  are sent to save. You will receive your encrypted messages and you can
-  decrypt them on the fly with different kexs for different messages if
+  are sent to the saving. You will receive your encrypted messages and you can
+  decrypt them on the fly with different keys for different messages, if
   you want.
 </p>
 
 <p class="welcome-paragraph">
-  At every entrance will be started a session that rappresents a day or a
+  At every will be assigned to you the session that rappresents a day or a
   "Work day", in that session you can set the goals of the days and then
-  change the goals status or delete them.
+  change the goals status or delete them. The session get changed when the user enter after 8 hurs to the last session.
 </p>
 
 <p class="welcome-paragraph">
-  When something important is in the way but you have to do something
+  When something important is new to do, but you have to do something
   right away you can set an alarm, ad you will get notifyed when the time
   is gone
 </p>
