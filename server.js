c50ee402b47ae3741efe2ad7e98efad36601a02f
Massimo Ganzerli <massimoganzerli@yahoo.com>
2019-01-16 15:03:15 +0100

    prepare for deploy

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require("./src/app");
 
-// define port
+// define port, process.env.PORT for heroku
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`app listening on port ${PORT}`));
